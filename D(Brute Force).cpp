39dba82758b738097202eebdc138a4fc397c17bf
Debadipto Biswas <49875064+JacNikola@users.noreply.github.com>
2022-01-07 16:47:24 +0530

    Update D(Brute Force).cpp

--- D(Brute Force).cpp
+++ D(Brute Force).cpp
@@ -8,56 +8,20 @@ int main()
     int illuminated[n + 1][m + 1];
     memset(illuminated, 0, sizeof(illuminated));
     illuminated[0][0] = 1;
-    int x = 0, y = 0, d = 0;
+    int curX = 0, curY = 0, velX = 1, velY = 1;
     while(1)
     {
-        if(d == 0)
-        {
-            x++; 
-            y++;
-        }
-        else if(d == 1)
-        {
-            x++;
-            y--;
-        }
-        else if(d == 2)
-        {
-            x--;
-            y--;
-        }
-        else if(d == 3)
-        {
-            x--;
-            y++;
-        }
- 
-        illuminated[x][y] = 1;
+        curX += velX;
+        curY += velY;
+     
+        illuminated[curX][curY] = 1;
  
         // Hits a corner
-        if((x == 0 || x == n) && (y == 0 || y == m)) break;
+        if((curX == 0 || curX == n) && (curY == 0 || curY == m)) break;
         
         // Hits a wall
-        if(x == 0)
-        {
-            if(d == 2) d = 1;
-            else if(d == 3) d = 0;
-        }
-        else if(x == n)
-        {
-            if(d == 0) d = 3;
-            else if(d == 1) d = 2;
-        }
-        else if(y == 0)
-        {
-            if(d == 1) d = 0;
-            else if(d == 2) d = 3;
-        }
-        else if(y == m)
-        {
-            if(d == 0) d = 1;
-            else if(d == 3) d = 2;
-        }
+        if(curX == n or curX == 0) velX *= -1;
+        if(curY == m or curY == 0) velY *= -1;      
     }
  
     int q; 
