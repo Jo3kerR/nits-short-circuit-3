ddd468127942b68db79bd2c51ebffba31e5acd99
Abhinab Saha <53913877+Jo3kerR@users.noreply.github.com>
2022-01-07 10:58:01 +0530

    Update D.cpp

--- D.cpp
+++ D.cpp
@@ -6,42 +6,38 @@ int main() {
     ios :: sync_with_stdio(0);
     cin.tie(0);
 
-    int t;
-    cin >> t;
-    while(t--) {
-        int n, m, q; cin >> n >> m >> q;
-        set<pair<int,int>> s;
-        int x = 0, y = 0 ;
-        int dir = 0;
-        while(1) {
-            s.insert({x, y});
-            if((x == 0 && y == m) || (x == n && (y == 0 || y == m))) break ;
-            if(dir == 0) {
-                if(x < n && y < m) ++x, ++ y;
-                else if(x == n) --x, ++y, dir = 3;
-                else ++x, -- y, dir = 1;
-            }
-            else if(dir == 1) {
-                if(x < n && y > 0) ++x, -- y;
-                else if(x == n) --x, --y, dir = 2;
-                else ++x, ++y, dir = 0;
-            }
-            else if(dir == 2) {
-                if(x > 0 && y > 0) --x, --y ;
-                else if(x == 0) ++x, -- y, dir = 1;
-                else --x, ++y, dir = 3;
-            }
-            else { 
-                if(x > 0 && y < m) --x, ++y ;
-                else if(x == 0) ++x, ++y, dir = 0;
-                else --x, -- y, dir = 2;
-            }
+    int n, m, q; cin >> n >> m >> q;
+    set<pair<int,int>> s;
+    int x = 0, y = 0 ;
+    int dir = 0;
+    while(1) {
+        s.insert({x, y});
+        if((x == 0 && y == m) || (x == n && (y == 0 || y == m))) break ;
+        if(dir == 0) {
+            if(x < n && y < m) ++x, ++ y;
+            else if(x == n) --x, ++y, dir = 3;
+            else ++x, -- y, dir = 1;
         }
-        while(q--) {
-            int x, y; cin >> x >> y;
-            if(s.find({x, y}) != s.end()) cout << "YES\n" ;
-            else cout << "NO\n" ;
+        else if(dir == 1) {
+            if(x < n && y > 0) ++x, -- y;
+            else if(x == n) --x, --y, dir = 2;
+            else ++x, ++y, dir = 0;
         }
+        else if(dir == 2) {
+            if(x > 0 && y > 0) --x, --y ;
+            else if(x == 0) ++x, -- y, dir = 1;
+            else --x, ++y, dir = 3;
+        }
+        else { 
+            if(x > 0 && y < m) --x, ++y ;
+            else if(x == 0) ++x, ++y, dir = 0;
+            else --x, -- y, dir = 2;
+        }
+    }
+    while(q--) {
+        int x, y; cin >> x >> y;
+        if(s.find({x, y}) != s.end()) cout << "YES\n" ;
+        else cout << "NO\n" ;
     }
 
     return 0;
